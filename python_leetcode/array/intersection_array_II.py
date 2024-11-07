fc403b354d71d13551cdb6fbe232db7122592f71
slau8405 <slau8405@uni.sydney.edu.au>
2024-11-07 23:26:37 +1100

    Added pyhton problem

--- python_leetcode/array/intersection_array_II.py
+++ python_leetcode/array/intersection_array_II.py
@@ -1,15 +1,30 @@
 """
 Problem: https://leetcode.com/problems/intersection-of-two-arrays-ii/
 """
-
-
-import numpy as np
-
+from typing import List
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        inter_sec = []
-        for i in nums1:
-            if i in nums2:
-                inter_sec.append(i)
-                nums2.remove(i)
-        return inter_sec
+        from collections import Counter
+        c1, c2 = Counter(nums1), Counter(nums2)
+        res = []
+        for k, v in c1.items():
+            if k in c2:
+                res.extend([k] * min(v, c2[k]))
+        return res
+    
+
+
+# Another solution
+
+from typing import List
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        from collections import Counter
+        c1, c2 = Counter(nums1), Counter(nums2)
+        return list((c1 & c2).elements())
+    
+
+# Another solution
+
+from typing import List
+
