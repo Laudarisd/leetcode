8cd7b5e46922286fa7af7402932c1268fe576bdf
slau8405 <slau8405@uni.sydney.edu.au>
2024-11-09 17:56:41 +1100

    Added pyhton problem

---
+++ python_leetcode/array/reshape_matrix.py
@@ -0,0 +1,10 @@
+"""
+
+Problem: https://leetcode.com/problems/reshape-the-matrix/description/
+"""
+
+
+from typing import List
+
+class Solution:
+        def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
