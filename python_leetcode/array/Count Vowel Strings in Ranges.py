8df6e01f4790c207140fe31ba82283b7b00da77b
Sudip Laudari <cadiankorea@gmail.com>
2025-01-02 12:43:15 +0900

    Added daily challenge

--- python_leetcode/array/Count Vowel Strings in Ranges.py
+++ python_leetcode/array/Count Vowel Strings in Ranges.py
@@ -11,11 +11,11 @@ Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
 
 
 from typing import List
-from bisect import bisect_left, bisect_right
+from bisect import bisect_left, bisect_right # bisect left and right are used to find the index of the first element that is greater than or equal to the target element
 
 
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        vowels = set('aeiou')
+        vowels = set("aeiou")
         nums = [i for i, w in enumerate(words) if w[0] in vowels and w[-1] in vowels]
-        return[bisect_left(nums, l) - bisect_left(nums, r) + int(words[r][0] in vowels and words[r][-1] in vowels) for l, r in queries]
+        return [bisect_right(nums, r) - bisect_left(nums, l) for l, r in queries]
